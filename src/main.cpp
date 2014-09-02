cfd7c2207f2ded49990c671e9a2f20fe88917939
The Keymaker <keymaker@valorbit.com>
2014-09-02 03:11:48 +0300

    Small fix printf type

--- src/main.cpp
+++ src/main.cpp
@@ -2547,7 +2547,7 @@ bool LoadBlockIndex(bool fAllowNew)
                     if (CheckProofOfWork(block.GetHash(), block.nBits)) {
                        printf("* Solved genesis block! nonce %u hash 0x%s time %u\n",
                          block.nNonce, block.GetHash().ToString().c_str(), block.nTime);
-                       printf("* Mining took %lu minutes\n", (GetTimeMillis() - nStart)/60000);
+                       printf("* Mining took %llu minutes\n", (GetTimeMillis() - nStart)/60000);
                     }
                           block.print();
             }
@@ -2649,7 +2649,7 @@ bool LoadBlockIndex(bool fAllowNew)
                     if (CheckProofOfWork(block.GetHash(), block.nBits)) {
                        printf("* Solved seed block! nonce %u hash 0x%s time %u\n",
                          block.nNonce, block.GetHash().ToString().c_str(), block.nTime);
-                       printf("* Mining took %lu minutes\n", (GetTimeMillis() - nStart)/60000);
+                       printf("* Mining took %llu minutes\n", (GetTimeMillis() - nStart)/60000);
                     }
                     block.print();
         }
